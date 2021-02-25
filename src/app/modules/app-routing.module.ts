8f11a08b485697881d157d2a4cb6ffc716e65c1b
IKTIKN <developermarc@outlook.com>
2021-02-25 01:38:48 +0100

    refactor

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'tradingview', component: TradingviewComponent },
   { path: 'marketview', component: MarketviewComponent },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
